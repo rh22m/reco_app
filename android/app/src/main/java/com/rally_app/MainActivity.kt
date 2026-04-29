b5f6f5b12c68b48c66b6cd1818dfe41ab77f1c78
rh22m <simonrheem@sungkyul.ac.kr>
2026-04-29 18:16:36 +0900

    앱 명칭 전면 수정

--- android/app/src/main/java/com/rally_app/MainActivity.kt
+++ android/app/src/main/java/com/rally_app/MainActivity.kt
@@ -11,8 +11,7 @@ class MainActivity : ReactActivity() {
    * Returns the name of the main component registered from JavaScript. This is used to schedule
    * rendering of the component.
    */
-  override fun getMainComponentName(): String = "rally_app"
-
+  override fun getMainComponentName(): String = "RECO" // 여기를 RECO로 수정
   /**
    * Returns the instance of the [ReactActivityDelegate]. We use [DefaultReactActivityDelegate]
    * which allows you to enable New Architecture with a single boolean flags [fabricEnabled]
--- index.js
+++ index.js
@@ -7,4 +7,4 @@ import App from './App'; // ./App.js 파일을 불러옵니다.
 import {name as appName} from './app.json'; // 'rally_app'이 아니라 'name as appName'으로 가져옵니다.
 
 // 앱 이름(appName)으로 App 컴포넌트를 등록합니다.
-AppRegistry.registerComponent(appName, () => App);
+AppRegistry.registerComponent('RECO', () => App);
